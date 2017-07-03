230e078f1fd98c5c4452b1b1d0a2dbded7561041
ashokkumaresan <ashok1989.stone@gmail.com>
2017-07-03 23:39:06 +0400

    Dairy insert and update functionality

--- pages/modules/Dairy.js
+++ pages/modules/Dairy.js
@@ -1,25 +1,71 @@
 var dairyService=(function(){
 	var insertDetails=function(data){
+		//alert(JSON.stringify(data));
 		var res=generic.service.invoke("generic","Ajax",this,[{"service":"http://localhost:3000/api/dairy","type":"POST","data":data,"callback":insertSuccess}]);
 	}
 
 	var insertSuccess=function(res){
-
+			alert("Insert Sccuess")
 	}
+
+		var updateDetails=function(data){
+		//alert(JSON.stringify(data));
+		var res=generic.service.invoke("generic","Ajax",this,[{"service":"http://localhost:3000/api/dairy","type":"PUT","data":data,"callback":updateSuccess}]);
+	}
+	var updateSuccess=function(res){
+		alert("Update Sccuess")
+	}
+
 	var loadDateTime=function(){
 		var d=new Date();
 		$('#txtdate').val(d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear());
 		$('#txttime').val(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
 	}
+	var checkDateEntry=function(){
+		var d=$('#txtdate').val();
+		generic.service.invoke("generic","Ajax",this,[{"service":"http://localhost:3000/api/checkDate","type":"GET","data":{"date":$('#txtdate').val()},"callback":successCheckDate}]);
+	}
+	var successCheckDate=function(res){
+		//alert('entry')
+		if(!res.status){
+			var _obj={
+				"date":$('#txtdate').val(),
+				"timeSlot":[
+				{
+					"time":$('#txttime').val(),
+					"content":$('#txtdetails').val()
+				}
+				]
+			}
+			//alert('before');
+			_obj=JSON.stringify(_obj);
+			insertDetails(_obj);
+		}
+		else{
+				var _obj={		
+				"find":{
+					"date":$('#txtdate').val()
+				},
+				"time":{		
+					"time":$('#txttime').val(),
+					"content":$('#txtdetails').val()	
+					}						
+			}
+			_obj=JSON.stringify(_obj);
+			updateDetails(_obj);
+		}
+	}
 	return{
 		insert:insertDetails,
 		load:loadDateTime,
+		check:checkDateEntry
 	}
 
 })();
 dairyService.load();
 $(document).ready(function(e){
 	$('#txtdairysubmit').click(function(e){
-		alert('hi');
+		//alert('hi');
+		dairyService.check();		
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express=require('express');
+var bodyParser=require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var router=express.Router();
 var mongo=require('mongodb');
-
+router.use(bodyParser.json());
 router.use(cookieParser());
 router.use(session({secret: "hotshot"}));
 
@@ -64,7 +65,8 @@ else{
 
 /*Start Dairy service*/
 router.post('/api/dairy',function(req,res){
-var query=req.query;
+	console.log("In Post");
+var query=req.body;
 query["username"]=req.session.User[0].username;
 console.log(query);
 MongoClient=mongo.MongoClient;
@@ -78,6 +80,60 @@ else{
 	collection.insert(query);
 }
 });
+});
+
+
+router.put('/api/dairy',function(req,res){
+	console.log("In Put");
+var query=req.body;
+query["username"]=req.session.User[0].username;
+console.log(query);
+MongoClient=mongo.MongoClient;
+var url='mongodb://localhost/SA';
+MongoClient.connect(url,function(err,db){
+if(err){
+	console.log("Error is getting data "+err);
+}
+else{
+	var collection=db.collection("dairies");
+	collection.update({"username":query.username,"date":query.find.date},{$push:{timeSlot:query.time}});
+}
+});
+});
+
+
+
+router.get('/api/checkDate',function(req,res){
+var query=req.query;
+	MongoClient=mongo.MongoClient;
+	var url='mongodb://localhost/SA';
+	MongoClient.connect(url,function(err,db){
+		if(err){
+			console.log("Error in connecting database");
+		}
+		else{
+			var collection=db.collection("dairies");
+			collection.find(query).toArray(function(err,result){
+				if(err){
+					console.log("Error is getting data "+err);
+				}
+				else{					
+					//res.json(result.length);
+					if(result.length>0){
+							res.header('Content-Length', 100);
+							var obj={"status":true};
+							res.json(obj);
+			//return res.redirect('http://localhost:3000/dashboard.html');
+					}else{							
+							res.header('Content-Length', 100);
+							var obj={"status":false};
+							res.json(obj);
+					}
+				}
+			});
+			db.close();
+		} 	
+	});
 });
 /*End Dairy service*/
 module.exports=router;
